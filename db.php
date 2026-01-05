460ecaa71b037546a102d898276c22a00a9341cd
alebri0616 <alebri0616@skola.goteborg.se>
2026-01-05 01:09:24 +0100

    adjusted the database

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 function getDatabase(): PDO
 {
-    $db = new PDO('sqlite:database.sql');
+    $db = new PDO('sqlite:database/hotel.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 
