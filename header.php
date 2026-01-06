2a49be97119fee050ad1b154930a9abbea929a3d
alebri0616 <alebri0616@skola.goteborg.se>
2026-01-06 20:56:34 +0100

    added link to bookingstyle.css to header

--- header.php
+++ header.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Magical Island Hotel - Paradise Awaits</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/bookingstyle.css">
 </head>
 <body>
     <div class="container">
